08aa37a1cae56365cdbdf7a8be0c42adbfefcf5f
Andrei <murguandrei2@gmail.com>
2024-03-04 15:32:21 +0200

    chore: small changes on the list view item

--- app/src/main/java/com/example/ledcontrollerproject/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/ledcontrollerproject/ui/home/HomeFragment.kt
@@ -1,45 +1,32 @@
 package com.example.ledcontrollerproject.ui.home
 
 import android.Manifest
-import android.app.Activity
+import android.annotation.SuppressLint
 import android.app.Activity.RESULT_OK
 import android.app.AlertDialog
-import android.app.admin.DevicePolicyManager
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
-import android.content.BroadcastReceiver
-import android.content.Context
 import android.content.Intent
-import android.content.IntentFilter
 import android.content.pm.PackageManager
-import android.net.Uri
 import android.os.Build
-import android.os.Build.VERSION
-import android.os.Build.VERSION.SDK_INT
 import android.os.Bundle
-import android.os.Parcelable
-import android.provider.Settings
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ListView
+import android.widget.SimpleAdapter
 import android.widget.Toast
 import androidx.activity.result.ActivityResult
-import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import androidx.core.content.ContextCompat.RECEIVER_VISIBLE_TO_INSTANT_APPS
-import androidx.core.content.ContextCompat.checkSelfPermission
-import androidx.core.content.ContextCompat.registerReceiver
-import androidx.core.content.getSystemService
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.ledcontrollerproject.R
 import com.example.ledcontrollerproject.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -47,7 +34,6 @@ class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
     private var btPermission = false
-    private lateinit var bluetoothAdapter: BluetoothAdapter
     private val devicesList: MutableList<BluetoothDevice> = mutableListOf()
     private lateinit var devicesAdapter: ArrayAdapter<String>
     private var isFound = true
@@ -61,33 +47,121 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        devicesAdapter = ArrayAdapter(
-            requireContext(),
-            android.R.layout.simple_list_item_1,
-            devicesList.map {
-                if (ActivityCompat.checkSelfPermission(
-                        requireContext(),
-                        Manifest.permission.BLUETOOTH_CONNECT
-                    ) != PackageManager.PERMISSION_GRANTED
-                ) {
-                    return binding.devicesListView
-                }
-                it.name ?: "Unknown Device"
-            })
-        val devicesListView: ListView = binding.devicesListView
-        devicesListView.adapter = devicesAdapter
 
-        val connectButton: Button = binding.connectToBluetooth
+         val connectButton: Button = binding.connectToBluetooth
+
         connectButton.setOnClickListener {
             Log.d("Bluetooth", "Connect button clicked. Initiating Bluetooth scan...")
-            scanBluetooth()
+            scanBt()
 
         }
 
         return root
     }
 
-    private fun scanBluetooth() {
+    fun scanBt() {
+        Log.d("Bluetooth", "Initiating Bluetooth scan...")
+        val bluetoothManager: BluetoothManager =
+            requireContext().getSystemService(BluetoothManager::class.java)
+        val bluetoothAdapter = bluetoothManager.adapter
+        if (bluetoothAdapter == null) {
+            Toast.makeText(requireContext(), "Device doesn't support Bluetooth", Toast.LENGTH_LONG)
+                .show()
+            Log.e("Bluetooth", "Device doesn't support Bluetooth")
+        } else {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                bluetoothPermissionLauncher.launch(Manifest.permission.BLUETOOTH_CONNECT)
+            } else {
+                Log.d(
+                    "Bluetooth",
+                    "Bluetooth adapter initialized. Requesting Bluetooth permission..."
+                )
+                bluetoothPermissionLauncher.launch(Manifest.permission.BLUETOOTH)
+            }
+        }
+    }
+
+    private val bluetoothPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        Log.d("Bluetooth", "Bluetooth permission granted: $isGranted")
+        if (isGranted) {
+            val bluetoothManager: BluetoothManager =
+                requireContext().getSystemService(BluetoothManager::class.java)
+            val bluetoothAdapter = bluetoothManager.adapter
+            btPermission = true
+            if (!bluetoothAdapter.isEnabled) {
+                val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                Log.d(
+                    "Bluetooth",
+                    "Bluetooth is not enabled. Launching Bluetooth activation intent..."
+                )
+                btActivityResultLauncher.launch(enableBtIntent)
+            } else {
+                scanBT()
+            }
+        } else {
+            btPermission = false
+        }
+    }
+    private val btActivityResultLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result: ActivityResult ->
+        Log.d("Bluetooth", "Bluetooth activation result: ${result.resultCode}")
+        if (result.resultCode == RESULT_OK) {
+            scanBT()
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun scanBT() {
+        val bluetoothManager: BluetoothManager =
+            requireContext().getSystemService(BluetoothManager::class.java)
+        val bluetoothAdapter = bluetoothManager.adapter
+        val builder = AlertDialog.Builder(this@HomeFragment.requireContext())
+        val inflater = layoutInflater
+        val dialogView: View = inflater.inflate(R.layout.scan_bt, null)
+        builder.setCancelable(false)
+        builder.setView(dialogView)
+        val btList = dialogView.findViewById<ListView>(R.id.devicesList)
+        val dialog = builder.create()
+        val pairedDevices: Set<BluetoothDevice> =
+            bluetoothAdapter?.bondedDevices as Set<BluetoothDevice>
+        val ADAhere: SimpleAdapter
+        var data: MutableList<Map<String?, Any?>?>? = null
+        data = ArrayList()
+        if (pairedDevices.isNotEmpty()) {
+            val datanum1: MutableMap<String?, Any> = HashMap()
+            datanum1["A"] = " "
+            datanum1["B"] = " "
+            data.add(datanum1)
+            for (device in pairedDevices) {
+                val datanum: MutableMap<String?, Any?> = HashMap()
+                datanum["A"] = device.name
+                datanum["B"] = device.address
+                data.add(datanum)
+            }
+            val fromwhere = arrayOf("A")
+            val viewswhere = intArrayOf(R.id.itemName)
+            ADAhere  = SimpleAdapter(requireContext(),data,R.layout.item_list,fromwhere,viewswhere)
+            btList.adapter = ADAhere
+            ADAhere.notifyDataSetChanged()
+            btList.onItemClickListener = AdapterView.OnItemClickListener { adapaterView, view, position, l ->
+                val string = ADAhere.getItem(position) as HashMap<String,String>
+                val deviceName = string["A"]
+                binding.deviceName.text = deviceName
+                dialog.dismiss()
+            }
+        } else {
+            val value = "No devices were found"
+            Toast.makeText(requireContext(), value, Toast.LENGTH_LONG).show()
+            return
+        }
+        dialog.show()
+    }
+
+
+    /*    private fun scanBluetooth() {
         Log.d("Bluetooth", "Initiating Bluetooth scan...")
         val bluetoothManager: BluetoothManager =
             requireContext().getSystemService(BluetoothManager::class.java)
@@ -227,10 +301,10 @@ class HomeFragment : Fragment() {
     inline fun <reified T : Parcelable> Intent.parcelable(key: String): T? = when {
         SDK_INT >= 33 -> getParcelableExtra(key, T::class.java)
         else -> @Suppress("DEPRECATION") getParcelableExtra(key) as? T
-    }
+    }*/
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-        requireActivity().unregisterReceiver(receiver)
+        //    requireActivity().unregisterReceiver(receiver)
     }
 }
